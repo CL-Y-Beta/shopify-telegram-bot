295b9bc9c7a0aa54629ae517e546ab34fb6037cd
Clarissa Yeo <Clarissayeo94@gmail.com>
2024-02-23 23:38:34 +0800

    Fixed some port issues.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import path from 'path';
 const app = express();
 
 
-const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+const port = process.env.PORT || 5000;
 
 // app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
@@ -63,5 +63,3 @@ app.get('/data', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-module.exports = app;
